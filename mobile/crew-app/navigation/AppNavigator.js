3ad5ab490fb63688ab67b8bb88a1da39e8c152c3
Umutbek <karimovumutbek@gmail.com>
2024-05-26 08:34:16 -0700

    Crew assigned job lists and job start-end added

---
+++ mobile/crew-app/navigation/AppNavigator.js
@@ -0,0 +1,24 @@
+// navigation/AppNavigator.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
+import JobDetailScreen from '../screens/JobDetailScreen';
+
+
+const Stack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="JobDetail" component={JobDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
